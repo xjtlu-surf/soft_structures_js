98236ac924e978c080c8772555fa78a7f341dd38
Martijn <m.t.bhomer@gmail.com>
2025-08-30 11:08:37 +0200

    Fix background

--- src/Background.tsx
+++ src/Background.tsx
@@ -10,7 +10,7 @@ export default function Background() {
         left: 0,
         width: '100vw',
         height: '100vh',
-        background: 'url(/back.png) center center / cover no-repeat',
+        background: 'url(/soft_structures_js/back.png) center center / cover no-repeat',
         filter: 'contrast(1.15) saturate(1.2) brightness(1.05)',
       }}
     />
